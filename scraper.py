63e7c5b792be3b203ca50a9a220ebaee41ee632d
arzoosingh24 <arzoosingh24@gmail.com>
2021-10-28 18:17:37 -0700

    invalid urls

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -13,6 +14,8 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
+    parsed = urlparse(url)
+    finalLinks = []
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
@@ -24,16 +27,34 @@ def extract_next_links(url, resp):
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     return list()
 
+def checkNetLoc(netloc, path):
+    if "ics.uci.edu" not in netloc and "cs.uci.edu" not in netloc and "informatics.uci.edu" not in netloc and "stat.uci.edu" not in netloc and "today.uci.edu" not in netloc:
+        return False # make sure that the netlocation is one of the ics sites
+    if "today.uci.edu" in netloc and "/department/information_computer_sciences" not in path:
+        return False # if the today.uci.edu doesnt have "/department/information_computer_sciences" in it 
+    if "archive.ics.uci.edu" in netloc or "grape.ics.uci.edu" in netloc or "intranet.ics.uci.edu" in netloc:
+        return False # we can't have archive or grape or intranet on there
+    if "wics.ics.uci.edu" in netloc and ("events" in path or "contact" in path):
+        return False
+    if "hack.ics.uci.edu" in netloc and "contact" in path:
+        return False
+    return True
+
+
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
+        
+        if parsed.scheme != "http" and parsed.scheme != "https":
             return False
-        if "ics.uci.edu" not in parsed.netloc and "cs.uci.edu" not in parsed.netloc and "informatics.uci.edu" not in parsed.netloc and "stat.uci.edu" not in parsed.netloc:
-            return False # make sure that the netlocation is one of the ics sites
+
+        if checkNetLoc(parsed.netloc, parsed.path) == False:
+            return False
+        
+        
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
